045cc9e1922cc0be353ef52f11a56df22edc8e3f
Juan J. Rojas <48027894+juanjorojash@users.noreply.github.com>
2024-10-07 21:52:46 -0600

    Update serial_read.py

--- algoritmos/BMP280/BMP280_lib/serial_read.py
+++ algoritmos/BMP280/BMP280_lib/serial_read.py
@@ -7,6 +7,8 @@ datos = []
 
 for i in range(10):
     dato = arduino.readline()[:-2].decode('utf-8').split(',')
-    datos.append([datetime.now().strftime(format="%Y-%m-%d %H:%M:%S"), float(dato[0]),float(dato[1])])
+    datos.append([datetime.now().strftime(format="%Y-%m-%d %H:%M:%S"),
+                  float(dato[0]),
+                  float(dato[1])])
 
 print(datos)
