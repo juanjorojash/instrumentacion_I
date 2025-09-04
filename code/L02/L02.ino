7588f9b637b2c676452e8508595eedca5c706250
Juan J. Rojas <juanjorojash@gmail.com>
2025-09-04 09:56:38 -0600

    ANNIE

--- code/L02/L02.ino
+++ code/L02/L02.ino
@@ -8,10 +8,10 @@ int min = 0;
 int hou = 0; 
 char time_stamp[50];
 
-#define RR 10240
+#define R0 10240
 #define BETA 4100
 #define T0 298.15
-#define R0 10000
+#define RR 10000
 #define zero 273.15
 
 void setup() {
@@ -25,13 +25,27 @@ void loop() {
   azero = analogRead(A0);
   // convertir en voltaje
   v = (azero/1023.0)*5;
-  // convertir en resistencia
+
+
+// convertir en resistencia
+  //Definir RT que es la relación entre R0 y R
   RT = (5/v) - 1;
+
+  //Despejar R (definirla otra vez como RT)
   RT = RR/RT;
-  // convertir en temperatura
-  T = BETA + T0 * log(RT/R0);
+
+  // convertir en temperatura con la ecuación proporcionada
+
+  // Despeje la ecuación para T
+
+  //  Simplifique la ecuación para obtener el numerador y denominador por separado
+
+  T = BETA + T0 * log(RT/RR);
   T = (BETA * T0) / T;
-  T = T - zero;
+
+  // Convertir a grados Celsius 
+ T = T - zero;
+
   sprintf(time_stamp,"%02d:%02d:%02d,", hou, min, sec);
   Serial.print(time_stamp);
   Serial.println(T); 
