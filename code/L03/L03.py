c67254897ab0b66ad0301edead367244a897c94b
Juan J. Rojas <juanjorojash@gmail.com>
2025-10-07 10:20:35 -0600

    cambio

--- code/L03/L03.py
+++ code/L03/L03.py
@@ -4,33 +4,6 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 from drawnow import drawnow
 
-# #Serial takes these two parameters: serial device and baudrate
-# ser = serial.Serial('COM8', 9600)
-
-# file = open("data.csv","w")
-# header = "timestamp,temperature\n"
-# file.write(header)
-
-# time_data = []
-# T_data = []
-
-# def data_fig():
-#     plt.plot(time_data,T_data)
-
-# with open("data.csv", "a", buffering=1) as file:
-#     while True:
-#         rawdata = (ser.readline().decode('ascii').strip('\n'))
-#         hou = int(rawdata[0:2])
-#         min = int(rawdata[3:5])
-#         sec = int(rawdata[6:8])
-#         time = hou*3600 + min*60 + sec
-#         T = float(rawdata[9:14])
-#         time_data.append(time)
-#         T_data.append(T)
-#         file.write(rawdata)
-#         print(rawdata)
-#         drawnow(data_fig)
-
 arduino = serial.Serial('COM14')
 nombre = "bmp280"
 narchivo = 1
@@ -61,7 +34,7 @@ seguir = input("Cualquier tecla")
 # Guardar en CSV
 with open(f"{nombre}_{narchivo}.csv", "w", newline="") as f:
     writer = csv.writer(f)
-    writer.writerow(["timestamp", "Temperatura", "Presión"])  # encabezados
+    writer.writerow(["timestamp", "Temperatura (°C)", "Presión (hPa)"])  # encabezados
     writer.writerows(datos)
 
 print("Datos guardados en mediciones.csv")
